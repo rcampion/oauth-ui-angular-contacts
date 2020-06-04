7995858a8d99043e63bb4c9bb11edd0b6b2b4101
Richard Campion <campion.richard@gmail.com>
2020-06-04 02:46:27 -0400

    Cleanup

--- src/app/core/services/post.service.ts
+++ src/app/core/services/post.service.ts
@@ -2,11 +2,7 @@ import { Injectable } from '@angular/core';
 import { SocketClientService } from '../socket-client.service';
 import { Observable } from 'rxjs/internal/Observable';
 import { Client } from '@stomp/stompjs';
-//import { environment } from '../../environments/environment';
-//import { PostListing } from './post-listing';
 import { first, map, switchMap } from 'rxjs/operators';
-//import { PostInfo } from './post-info';
-//import { PostInput } from './post-input';
 import { Contact } from '../interface/contact.model';
 
 @Injectable({
@@ -16,25 +12,12 @@ export class PostService {
 
   constructor(private socketClient: SocketClientService) {
   }
-/*
-  findAll(): Observable<PostListing[]> {
-    return this.socketClient
-      .onMessage('/topic/posts/get')
-      .pipe(first(), map(posts => posts.map(PostService.getPostListing)));
+
+  static getPostListing(post: any): Contact {
+    const postedAt = new Date(post['postedAt']);
+    return {...post, postedAt};
   }
 
-  findOne(id: number): Observable<PostInfo> {
-    return this.socketClient
-      .onMessage(`/topic/posts/${id}/get`)
-      .pipe(first(), map(post => PostService.getPostInfo(post)));
-  }
-
-  findByAuthor(username: string): Observable<PostListing[]> {
-    return this.socketClient
-      .onMessage(`/topic/author/${username}/posts/get`)
-      .pipe(first(), map(posts => posts.map(PostService.getPostListing)));
-  }
-*/
   save(post: Contact) {
     return this.socketClient.send('/topic/posts/create', post);
   }
@@ -43,20 +26,4 @@ export class PostService {
     return this.socketClient.onMessage('/topic/posts/created').pipe(map(post => PostService.getPostListing(post)));
   }
 
-  static getPostListing(post: any): Contact {
-    const postedAt = new Date(post['postedAt']);
-    return {...post, postedAt};
-  }
-/*
-  static getPostInfo(post: any): PostInfo {
-    const postedAt = new Date(post['postedAt']);
-    const comments = post['comments'].map(comment => PostService.getComment(comment));
-    return {...post, postedAt, comments};
-  }
-
-  static getComment(comment: any): Comment {
-    const postedAt = new Date(comment['postedAt']);
-    return {...comment, postedAt};
-  }
-*/
 }
