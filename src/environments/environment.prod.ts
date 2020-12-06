3d1d2a0368a9ce0fc885d12a60747a534404d852
Richard Campion <campion.richard@gmail.com>
2020-12-05 19:07:53 -0500

    Change the backend URLs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,20 @@ export const environment = {
     production: true,
     redirectUri: 'https://www.zdslogic.com/contacts/about',
     sso_url: 'https://www.zdslogic.com/keycloak/auth',
-    api_url: 'https://www.zdslogic.com/resource-server/api',
+    // api_url: 'https://www.zdslogic.com/resource-server/api',
     // api_url: 'http://aws.richardcampion.com:9090/spring-boot-docker-server/api'
-    ws_url: 'https://www.zdslogic.com/resource-server/live'
+    // ws_url: 'https://www.zdslogic.com/resource-server/live'
+
+  // api_url: 'http://www.zdslogic-development.com:8080/ng8-spring-server/api'
+  // api_url: 'http://www.zdslogic-development.com:8080/spring-boot-docker-server/api'
+  // api_url: 'https://www.zdslogic.com/ng8-spring-server/api'
+  // api_url: 'http://localhost:8080/ng9-spring-server/api'
+
+  // redirectUri: 'https://www.zdslogic.com',
+  // sso_url: 'http://localhost:18080/auth',
+  // sso_url: 'https://www.zdslogic.com/keycloak/auth',
+  // api_url: 'http://localhost:8082/dashboard-server/api',
+  api_url: 'https://www.zdslogic.com/dashboard-server/api',
+  // ws_url: 'http://localhost:8082/dashboard-server/live'
+  ws_url: 'https://www.zdslogic.com/dashboard-server/live'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,11 +8,18 @@ export const environment = {
 //  api_url: 'http://www.zdslogic-development.com:8080/spring-boot-docker-server/api'
 //  api_url: 'https://www.zdslogic.com/ng8-spring-server/api'
 //  api_url: 'http://localhost:8080/ng9-spring-server/api'
+//  sso_url: 'http://localhost:18080/auth',
+//  api_url: 'http://www.zdslogic-development.com:8080/ng8-spring-server/api'
+//  api_url: 'http://www.zdslogic-development.com:8080/spring-boot-docker-server/api'
+//  api_url: 'https://www.zdslogic.com/ng8-spring-server/api'
+//  api_url: 'http://localhost:8080/ng9-spring-server/api'
   redirectUri: 'http://localhost:8089/home',
-  // sso_url: 'http://localhost:18080/auth',
+//  sso_url: 'http://localhost:18080/auth',
   sso_url: 'https://www.zdslogic.com/keycloak/auth',
-  api_url: 'http://localhost:8081/resource-server/api',
-  ws_url: 'http://localhost:8081/resource-server/live'
+  api_url: 'http://localhost:8082/dashboard-server/api',
+// api_url: 'https://www.zdslogic.com/dashboard-server/api',
+  ws_url: 'http://localhost:8082/dashboard-server/live'
+//ws_url: 'https://www.zdslogic.com/dashboard-server/live'
 };
 
 /*
