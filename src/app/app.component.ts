1cf7ea1fa3a8b6377e91c3ef331478aba5466a4f
Richard Campion <campion.richard@gmail.com>
2020-12-14 17:48:31 -0500

    Modify Header and Add Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,18 +4,21 @@ import { Router } from '@angular/router';
 import { UsersService } from './core/services/users.service';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 
 export class AppComponent implements OnInit {
-    title = 'ng9-contacts';
-    userService: UsersService;
+  title = 'ng9-contacts';
+  userService: UsersService;
+
+  date: string;
 
   constructor(router: Router, userService: UsersService) {
     const self = this;
     this.userService = userService;
+    this.date = this.getDate();
   }
 
   ngOnInit() {
@@ -27,6 +30,22 @@ export class AppComponent implements OnInit {
   logout(): void {
     this.userService.logout();
   }
+  
+  getDate() {
+    var theDate = new Date();
+    var theTime = theDate.getTime();
+    var months = new Array("January", "February", "March",
+      "April", "May", "June", "July", "August",
+      "September", "October", "November", "December");
+    //Ensure correct for language. English is "January 1, 2020"
+    var TODAY = months[theDate.getMonth()] + " "
+      + theDate.getDate() + ", " + theDate.getFullYear();
+    var DATETIME = months[theDate.getMonth()] + " "
+      + theDate.getDate() + ", " + theDate.getFullYear()
+      + ", " + theTime;
+    var DAYS = (((((theTime / 1000) / 60) / 60) / 24) / 365);
 
+    return TODAY;
+  }
 }
 
